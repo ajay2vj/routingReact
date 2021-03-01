0eabb2836c41cb556add7eefd47393d9e5dc164c
Ajay M <ajay@scodesoft.com>
2021-03-01 16:56:52 +0530

    add alt shoe

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,7 @@ class Home extends Component {
               </div>
               <div className="col-md-4">
                 <div className="product-image" data-aos="fade-up" data-aos-delay="500">
-                  <img src={window.location.origin + '/images/product-1.webp'} width="100%" alt="image"/>
+                  <img src={window.location.origin + '/images/product-1.webp'} width="100%" alt="shoe"/>
                   <div className="card-body">
                     <p>desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
                   </div>
@@ -26,7 +26,7 @@ class Home extends Component {
               </div>
               <div className="col-md-4">
                 <div className="product-image" data-aos="fade-up" data-aos-delay="500">
-                  <img src={window.location.origin + '/images/product-2.webp'} width="100%" alt="image"/>
+                  <img src={window.location.origin + '/images/product-2.webp'} width="100%" alt="shoe"/>
                   <div className="card-body">
                     <p>desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
                   </div>
@@ -34,7 +34,7 @@ class Home extends Component {
               </div>
               <div className="col-md-4">
                 <div className="product-image" data-aos="fade-up" data-aos-delay="500">
-                  <img src={window.location.origin + '/images/product-3.webp'} width="100%" alt="image"/>
+                  <img src={window.location.origin + '/images/product-3.webp'} width="100%" alt="shoe"/>
                   <div className="card-body">
                     <p>desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
                   </div>
@@ -51,12 +51,12 @@ class Home extends Component {
               </div>
               <div className="col-md-6">
                 <div className="left-side-column" data-aos="fade-up" data-aos-delay="500">
-                  <img src={window.location.origin + '/images/about.jpeg'} width="100%" alt="image"/>
+                  <img src={window.location.origin + '/images/about.jpeg'} width="100%" alt="shoe"/>
                 </div>
               </div>
               <div className="col-md-6">
                 <div className="right-side-column" data-aos="fade-up" data-aos-delay="500">
-                  <p><img src={window.location.origin + '/images/icon.png'} alt="image"/></p>
+                  <p><img src={window.location.origin + '/images/icon.png'} alt="shoe"/></p>
                   <h2 className="text-center">Shoe is an art</h2>
                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 </div>
