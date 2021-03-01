31501918f6285c552c061386d1b1759e98a1890b
Ajay M <ajay@scodesoft.com>
2021-03-01 16:53:01 +0530

    add alt property

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Footer from '../components/Footer.jsx';
 import Navbar from '../components/Navbar.jsx';
-import Jumbotron from '../components/Jumbotron.jsx';
 import Sidebar from '../components/Sidebar.jsx';
 
 class Home extends Component {
@@ -19,7 +18,7 @@ class Home extends Component {
               </div>
               <div className="col-md-4">
                 <div className="product-image" data-aos="fade-up" data-aos-delay="500">
-                  <img src={window.location.origin + '/images/product-1.webp'} width="100%"/>
+                  <img src={window.location.origin + '/images/product-1.webp'} width="100%" alt="image"/>
                   <div className="card-body">
                     <p>desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
                   </div>
@@ -27,7 +26,7 @@ class Home extends Component {
               </div>
               <div className="col-md-4">
                 <div className="product-image" data-aos="fade-up" data-aos-delay="500">
-                  <img src={window.location.origin + '/images/product-2.webp'} width="100%"/>
+                  <img src={window.location.origin + '/images/product-2.webp'} width="100%" alt="image"/>
                   <div className="card-body">
                     <p>desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
                   </div>
@@ -35,7 +34,7 @@ class Home extends Component {
               </div>
               <div className="col-md-4">
                 <div className="product-image" data-aos="fade-up" data-aos-delay="500">
-                  <img src={window.location.origin + '/images/product-3.webp'} width="100%"/>
+                  <img src={window.location.origin + '/images/product-3.webp'} width="100%" alt="image"/>
                   <div className="card-body">
                     <p>desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
                   </div>
@@ -52,12 +51,12 @@ class Home extends Component {
               </div>
               <div className="col-md-6">
                 <div className="left-side-column" data-aos="fade-up" data-aos-delay="500">
-                  <img src={window.location.origin + '/images/about.jpeg'} width="100%" />
+                  <img src={window.location.origin + '/images/about.jpeg'} width="100%" alt="image"/>
                 </div>
               </div>
               <div className="col-md-6">
                 <div className="right-side-column" data-aos="fade-up" data-aos-delay="500">
-                  <p><img src={window.location.origin + '/images/icon.png'} /></p>
+                  <p><img src={window.location.origin + '/images/icon.png'} alt="image"/></p>
                   <h2 className="text-center">Shoe is an art</h2>
                   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 </div>
